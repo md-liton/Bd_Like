e02faadce0e81d65c5e0323995da83f3bfda35d3
md-liton <mdliton5506@gmail.com>
2022-09-23 02:11:39 +0600

    bd_like

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function() {
+    // slider
+    $('.main_div').slick({
+        autoplay :true,
+        autoplaySpeed : 3000,
+        arrows : false,
+      });
+      // count_up
+      $('.count_up').counterUp({
+        delay: 10,
+        time: 1000
+      });
+    // father icon js start
+    feather.replace()
+    // father icon js end
+});
+
